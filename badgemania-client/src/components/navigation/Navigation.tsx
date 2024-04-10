edd5a135d43941675bce86506d2a8aabb424007c
Alexander Välitalo <143487921+AlexanderValitalo@users.noreply.github.com>
2024-04-10 18:03:42 +0200

    Add onClick

--- badgemania-client/src/components/navigation/Navigation.tsx
+++ badgemania-client/src/components/navigation/Navigation.tsx
@@ -1,28 +1,56 @@
+"use client";
+
+import { FC, useState } from "react";
 import { RouteLink } from "@/interfaces/rout-link";
 import Link from "next/link";
 
 const LINKS: RouteLink[] = [
   { href: "/", text: "Home" },
-  { href: "/students", text: "Students" },
-  { href: "/badgegroups", text: "Badgegroups" },
-  { href: "/badges", text: "Badges" },
+  { href: "/solutions", text: "Solutions" },
+  { href: "/pricing", text: "Pricing" },
+  { href: "/about", text: "About" },
   { href: "/contact", text: "Contact" },
 ];
 
 export default function Navigation() {
+  const [highlightedLink, setHighlightedLink] = useState<string>("/");
+
+  const handleNavClick = (linkButtonName: string) => {
+    setHighlightedLink(linkButtonName);
+  };
+
   return (
     <div className="flex justify-evenly gap-2 m-5">
       {LINKS.map((link) => (
-        <NavigationLink key={link.href} {...link} />
+        <NavigationLink
+          key={link.href}
+          link={link}
+          isSelected={highlightedLink === link.href}
+          onClick={() => handleNavClick(link.href)}
+        />
       ))}
     </div>
   );
 }
 
-function NavigationLink({ href, text }: RouteLink) {
+interface NavigationLinkProps {
+  link: RouteLink;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+const NavigationLink: FC<NavigationLinkProps> = ({ link, isSelected, onClick }) => {
   return (
-    <Link href={href}>
-      <h2>{text}</h2>
+    <Link
+      onClick={onClick}
+      href={link.href}
+      className={
+        isSelected
+          ? "m-24"
+          : "group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      }
+    >
+      <h2>{link.text}</h2>
     </Link>
   );
-}
+};
